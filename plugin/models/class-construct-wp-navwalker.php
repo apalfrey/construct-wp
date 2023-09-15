1dc923b31a2643d2059cccbf12d3b2473a36b5ac
Andrew Palfrey <apalfrey@apalfrey.me>
2023-09-15 21:14:06 +0100

    Fixed navwalker filter

--- plugin/models/class-construct-wp-navwalker.php
+++ plugin/models/class-construct-wp-navwalker.php
@@ -112,7 +112,7 @@ class CWP_Navwalker extends Walker_Nav_Menu {
 
         if ( $menu_item->logout_link == '1' ) {
             // TODO re-work with page definition in back end?
-            $logout_redirect = add_filter( 'cwp_logout_redirect', get_home_url() );
+            $logout_redirect = apply_filters( 'cwp_logout_redirect', get_home_url() );
             $menu_item->url  = wp_logout_url( $logout_redirect );
         }
 
