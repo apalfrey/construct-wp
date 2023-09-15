1b4d0365090f8306fd7b8fc57048673a6928dddb
Andrew Palfrey <apalfrey@apalfrey.me>
2023-09-15 21:14:47 +0100

    Removed unnecessary filters

--- plugin/models/class-construct-wp.php
+++ plugin/models/class-construct-wp.php
@@ -140,15 +140,6 @@ class Construct_WP {
             remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
             remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
         } );
-
-        // Disables the block editor from managing widgets in the Gutenberg plugin.
-        add_filter( 'gutenberg_use_widgets_block_editor', '__return_false', 100 );
-
-        // Disables the block editor from managing widgets. renamed from wp_use_widgets_block_editor.
-        add_filter( 'use_widgets_block_editor', '__return_false' );
-
-        // Disable contact form 7 styling.
-        add_filter( 'wpcf7_load_css', '__return_false' );
     }
 
     /**
